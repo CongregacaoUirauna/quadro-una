3813504161289df8e6f2b8b6d8fe1e529ee7d042
CongregacaoUirauna <congregacaouirauna@gmail.com>
2026-04-28 10:47:53 -0300

    Update modulo-acessos.js

--- js/modulo-acessos.js
+++ js/modulo-acessos.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { doc, setDoc, getDocs, collection, deleteDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { doc, setDoc, getDocs, collection, deleteDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { db, firebaseConfig } from './firebase-config.js';
 
 // Truque Sênior: Inicializa um segundo app para gerenciar novos usuários
